d7aefc8a7cfd659bf6d1deb3633c67a0e7fe20d0
subhranshudev <subhranshusekhar2509@gmail.com>
2025-06-21 07:47:00 +0530

    Selection sort done

--- sorting.cpp
+++ sorting.cpp
@@ -34,12 +34,33 @@ void bubbleSort(int *arr, int n){
     printArr(arr, n);
 }
 
+void selsectionSort(int *arr, int n){
+    for (int i = 0; i < n-1; i++)
+    {
+        int minIndex = i;
+        for (int j = i+1; j < n; j++)
+        {
+            if (arr[j] < arr[minIndex])
+            {
+                minIndex = j;
+            }  
+        }
+        swap(arr[i], arr[minIndex]);
+        
+    }
+    printArr(arr, n);
+    
+}
+
+
+
+
+
 int main(){
     int arr[] = {5, 4, 1, 3, 2};
     int n = sizeof(arr) / sizeof(int);
-    bubbleSort(arr, n);
-   
-    
+   // bubbleSort(arr, n);
+    selsectionSort(arr, n);
     
 
 
