0901f7a4b73c99c0903bbabadf03434707e973e6
subhranshudev <subhranshusekhar2509@gmail.com>
2025-06-28 23:44:18 +0530

    uppercase-lowercase conversion, reverse char array and valid palindrome or not done

--- strings.cpp
+++ strings.cpp
@@ -2,6 +2,64 @@
 #include<string.h> // or <cstring>
 using namespace std;
 
+void toUpperCase(char word[], int n){
+    for (int i = 0; i < n; i++)
+    {
+       char ch = word[i];
+       if (ch>= 'A' && ch<= 'Z')
+       {
+            continue;
+       } else{
+            word[i] = ch - 'a' + 'A';
+       }
+       
+    }
+    
+}
+
+void toLowerCase(char word[], int n){
+    for (int i = 0; i < n; i++)
+    {
+       char ch = word[i];
+       if (ch>= 'a' && ch<= 'z')
+       {
+            continue;
+       } else{
+            word[i] = ch - 'A' + 'a';
+       }
+       
+    }
+    
+}
+
+void reverse(char word[], int n){
+    int start = 0, end = n-1;
+    while (start< end)
+    {
+       swap(word[start], word[end]);
+       start++;
+       end--;
+    }
+}
+
+bool isdPalindrome(char word[], int n){
+    int start = 0, end = n-1;
+    while(start < end){
+        if (word[start] != word[end])
+        {
+            cout<< "Not a valid palindrome"<< endl;
+            return false;
+        } 
+        start++;
+        end--;
+    }
+    cout << "Valid Palindrome";
+    return true;
+}
+
+
+
+
 int main(){
 /*  // Basics 
     char word[] = { 'c', 'o', 'd', 'e', '\0'};
@@ -12,7 +70,7 @@ int main(){
     cout<< strlen(word2)<< endl;
 */
 
-    // Taking inputs
+/*    // Taking inputs
     // char word[30]; 
     // cin>> word;  // It will ignore all the inputs after a whitespace
     // cout << "Your word is: "<< word<< endl;
@@ -24,6 +82,28 @@ int main(){
                                     // char given in 3rd parameter(3rd parameter called delimiter is optional)
     cout << "Your sentence is: "<< sentence<< endl;
     cout<< "Length of sentence is: "<< strlen(sentence)<< endl;
+*/
+
+/*    // Convert to UpperCase
+    char word[] = "ApPle";
+    toUpperCase(word, strlen(word));
+    cout<< word<< endl;
+    // Convert to Lowercase
+    char word2[] = "dHaNi";
+    toLowerCase(word2, strlen(word2));
+    cout<< word2<< endl;
+*/
+
+/*    // Reverse a Character array
+    char word[] = "code";
+    reverse(word, strlen(word));
+    cout<< word<< endl;
+*/
+
+/*    // Valid Palindrome or not
+    char word[] = "racecar";
+    isdPalindrome(word, strlen(word));
+*/
 
     return 0;
 }
