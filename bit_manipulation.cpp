2039d3e67fe562f80f4b0c4707dfb7f31b62a4ae
subhranshudev <subhranshusekhar2509@gmail.com>
2025-06-30 23:56:39 +0530

    Bitwise Opeartors and One's Complement done

---
+++ bit_manipulation.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+/*    // Bitwise Operators
+    cout<< (3 & 5)<< endl; 
+    cout<< (3 | 5)<< endl;
+    cout<< (3 ^ 5)<< endl;
+*/
+
+    // One's Complement
+    cout << (~6)<< endl;
+    cout << (~0)<< endl;
+
+
+
+
+
+    return 0;
+}
