2ae42880bded40ffca81e43628477339182f51b4
subhranshudev <subhranshusekhar2509@gmail.com>
2025-08-21 23:32:44 +0530

    find subsets and permutations done

--- backtracking.cpp
+++ backtracking.cpp
@@ -19,13 +19,43 @@ void changeArr(int arr[], int n, int i){
     arr[i] -= 2; // backtracking
 }
 
+void subsets(string str, string subset){
+    if (str.size() == 0){
+        cout<< subset<< endl;
+        return;
+    }
+
+    char ch = str[0];
+    subsets(str.substr(1, str.size()-1), subset+ch); // yes choice
+    subsets(str.substr(1, str.size()-1), subset); // No choice
+}
+
+void permutations(string str, string permStr){
+    int n = str.size();
+    if(n == 0){
+        cout<< permStr<< endl;
+        return;
+    }
+
+    for(int i = 0; i< str.size(); i++){
+        char ch = str[i];
+        //permStr = permStr + str[i];
+        string nextStr = str.substr(0, i) + str.substr(i+1, n-i-1);
+        permutations(nextStr, permStr + ch);
+    }
+}
+
 int main(){
     int arr[5];
     int n = 5;
-    changeArr(arr, n, 0);
-    printArr(arr, n);
+    // changeArr(arr, n, 0);
+    // printArr(arr, n);
 
+    string str = "abc";
+    subsets(str, "");
 
-
+    string str = "abc";
+    string ans = "";
+    permutations(str, ans);
     return 0;
 }
