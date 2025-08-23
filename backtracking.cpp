09a635309399e4d088387c2e0102d247f06437cf
subhranshudev <subhranshusekhar2509@gmail.com>
2025-08-23 16:19:52 +0530

    Sudoku solver done

--- backtracking.cpp
+++ backtracking.cpp
@@ -124,7 +124,7 @@ int gridWays(int row, int col, int n, int m, string move){   // --> TC = O(2^(m+
     return val1 + val2; // Add the number of ways if moved right and moved down AND return
 }
 
-// Generate Parentheses --> Leetcode --> 22
+// Generate Parentheses --> Leetcode -22
 bool isSafeforClose(string str, int i, int n){
     int openPcount = 0, closePcount = 0;
     for(int j = 0; j < i; j++){
@@ -173,6 +173,72 @@ void wellParentheses(string str, int n,  int i){
    
 }
 
+// Sudoku Solver
+void printSudoku(int sudoku[][9]){
+    for(int i = 0; i< 9; i++){
+        for(int j = 0; j< 9; j++){
+            cout<< sudoku[i][j]<< " ";
+        }
+        cout<< endl;
+    }
+}
+
+bool isSafe(int sudoku[][9], int row, int col, int digit){
+    // horizontal
+    for(int j = 0; j< 9; j++){
+        if(sudoku[row][j] == digit){
+            return false;
+        }
+    }
+
+    // vertical
+    for(int i = 0; i< 9; i++){
+        if(sudoku[i][col] == digit){
+            return false;
+        }
+    }
+
+    // minor grid (3x3)
+    int startingRow = (row/3) * 3 ;
+    int startingCol = (col/3) * 3 ;
+    for(int i = startingRow; i<= startingRow+2; i++){
+        for(int j = startingCol; j<= startingCol+2; j++){
+            if(sudoku[i][j] == digit){
+                return false;
+            }
+        }
+    }
+
+    return true;
+}
+
+bool sudokuSolver(int sudoku[9][9], int row, int col){
+    if(row == 9){
+        printSudoku(sudoku);
+        return true;
+    }
+
+    int nextRow = row;
+    int nextCol = col + 1;
+    if(col+1 == 9){
+        nextRow = row + 1;
+        nextCol = 0;
+    }
+
+    if(sudoku[row][col] != 0){
+        return sudokuSolver(sudoku, nextRow, nextCol);
+    }
+    for(int digit = 1; digit<=9; digit++){
+        if(isSafe(sudoku, row, col, digit)){
+            sudoku[row][col] = digit;
+            if(sudokuSolver(sudoku, nextRow, nextCol)){
+                return true;
+            }
+            sudoku[row][col] = 0;
+        }
+    }
+    return false;
+}
 
 int main(){
     int arr[5];
@@ -188,7 +254,7 @@ int main(){
     // permutations(str, ans);
 
 /*    // N-Queens --> TC = O(n!), SC = O(n^2)
-    vector<vector<char>> board;
+    vector<vector<char>> sudoku;
     int n = 4;
 
     for(int i = 0; i< n; i++){
@@ -212,11 +278,24 @@ int main(){
     cout<< "Total grid ways = "<< gridWays(0, 0 , n, m, move)<< endl;
 */
 
-    // Generate Parentheses --> Leetcode --> 22
+/*    // Generate Parentheses --> Leetcode - 22
     string str = "(";
     int n = 3, i = 1;
     wellParentheses(str, n, i);
+*/
 
+    // Sudoku Solver
+    int sudoku[9][9] = {{0, 0, 8, 0, 0, 0, 0, 0, 0},
+                        {4, 9, 0, 1, 5, 7, 0, 0, 2},
+                        {0, 0, 3, 0, 0, 4, 1, 9, 0},
+                        {1, 8, 5, 0, 6, 0, 0, 2, 0},
+                        {0, 0, 0, 0, 2, 0, 0, 6, 0},
+                        {9, 6, 0, 4, 0, 5, 3, 0, 0},
+                        {0, 3, 0, 0, 7, 2, 0, 0, 4},
+                        {0, 4, 9, 0, 3, 0, 0, 5, 7},
+                        {8, 2, 7, 0, 0, 9, 0, 1, 3}  
+                        };
+    sudokuSolver(sudoku, 0, 0);
 
     return 0;
 }
