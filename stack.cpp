7c19a548387c7c08f08f04d8d149f827620f1318
subhranshudev <subhranshusekhar2509@gmail.com>
2025-09-29 13:01:36 +0530

    Stacks part 2 complete

--- stack.cpp
+++ stack.cpp
@@ -133,7 +133,7 @@ void printStack(stack<int> s){
     cout<< '\n';
 }
 
-void stockSpanProblem(vector<int> stock, vector<int> span){
+void stockSpanProblem(vector<int> stock, vector<int> &span){    // TC = O(n)
     stack<int> s;
     s.push(0);
     span[0] = 1;
@@ -158,9 +158,155 @@ void stockSpanProblem(vector<int> stock, vector<int> span){
     }
 }
 
+void nextGreaterElement(vector<int> arr, vector<int> &ans){     // TC = O(n)
+    stack<int> s;
+    int n = arr.size();
+    ans[n-1] = -1;
+    s.push(arr[n-1]);
+
+    for(int i = n-2; i >= 0; i--){
+        int currVal = arr[i];
+        while(!s.empty() && s.top() <= currVal){
+            s.pop();
+        }
+
+        if(s.empty()){
+            ans[i] = -1;
+        }else{
+            int nextHigh = s.top();
+            ans[i] = nextHigh;
+        }
+
+        s.push(currVal);
+    }
+
+    for(int i = 0; i< ans.size(); i++){
+        cout<< ans[i]<< " ";
+    }
+}
+
+bool validParenthesis(string str){  // TC =  O(n), SC = O(n)
+    stack<char> s;
+    for(int i = 0; i<str.size(); i++){
+        char ch = str[i];
+        if(ch == '(' || ch == '{' || ch == '['){
+            s.push(ch);
+        }
+
+        if(ch == ')' || ch == '}' || ch == ']'){
+            if(s.empty()){  // Insufficient opening bracket
+                return false;
+            }
+            char top = s.top();
+            if((top == '(' && ch == ')') || 
+               (top == '{' && ch == '}') || 
+               (top == '[' && ch == ']'))
+            {
+                s.pop();
+                continue;
+            }else{
+                return false;
+            }
+        }
+    }
+
+    if(s.empty()){
+        cout<< "Valid Parenthesis \n";
+        return true;
+    }else{  // Insufficient closing bracket
+        cout<< "Invalid Parenthesis \n";
+        return false;
+    }
+    
+}
+
+bool isDuplicateParenthesis(string str){
+    stack<char> s;
+
+    for(int i = 0; i< str.size(); i++){     //TC = O(n), SC = O(n)
+        char ch = str[i];
+        
+        if(ch != ')'){
+            s.push(ch);
+        } else{
+            if(s.top() == '(' ){
+                cout<< "Duplicate Parenthesis \n";
+                return true;
+            }
+
+            while(s.top() != '('){
+                s.pop();
+            }
+            s.pop();
+        }
+    }
+
+    cout<< "No duplicate parenthesis \n";
+    return false;
+}
+
+int maxAreaHistogram(vector<int> heights){   // TC = O(n)
+    int n = heights.size();
+    vector<int> nls(n);    // Next Left Smaller
+    vector<int> nrs(n);    // Next Right Smaller
+    stack<int> s;
+   
+    // Next Left Smaller
+    s.push(0);
+    nls[0] = -1;
+    for(int i = 1; i< n; i++){
+        int currVal = heights[i];
+        while(!s.empty() && heights[s.top()] >= currVal){
+            s.pop();
+        }
+
+        if(s.empty()){
+            nls[i] = -1;
+        } else{
+            nls[i] = s.top();
+        }
+
+        s.push(i);
+    }
+
+    // Vacate the stack for use in 2nd 
+    while(!s.empty()){
+        s.pop();
+    }
+
+    // Next Right Smaller
+    nrs[n-1] = n;
+    s.push(n-1);
+    for(int i = n-2; i >= 0; i--){
+        int currVal = heights[i];
+
+        while(!s.empty() && heights[s.top()] >= currVal){
+            s.pop();
+        }
+
+        if(s.empty()){
+            nrs[i] = n;
+        } else{
+            nrs[i] = s.top();
+        }
+
+        s.push(i);
+    }
+
+    // Calculate area and maxArea
+    int area = 0, maxArea = 0;
+    for(int i = 0 ; i < n; i++){
+        int width = nrs[i] - nls[i] - 1;
+        area = (heights[i] * width);
+        maxArea = max(area, maxArea);
+    }
+
+    return maxArea;
+}
+
 
 int main(){
-/*    // stack implementation using vector
+/*  // stack implementation using vector
     Stack s;
     s.push(3);
     s.push(2);
@@ -202,7 +348,8 @@ int main(){
     }
 */
 
-/*    tempStackll<int> s2;
+/*  // stack implementation using Linked List
+    tempStackll<int> s2;
     s2.push(3);
     s2.push(2);
     s2.push(1);
@@ -213,7 +360,8 @@ int main(){
     }
 */
 
-/*    stack<int> s;
+/*  // Push at bottom of stack
+    stack<int> s;
     s.push(3);
     s.push(2);
     s.push(1);
@@ -225,10 +373,12 @@ int main(){
     }
 */
 
+    // Reverse a string using stack
     // string str = "abcd";
     // cout<< reverseString(str);
 
-/*    stack<int> s;
+/* // Reverse a stack
+   stack<int> s;
     s.push(3);
     s.push(2);
     s.push(1);
@@ -237,9 +387,35 @@ int main(){
     printStack(s);
 */
 
+/*    // Stock span problem
     vector<int> stock = {100, 80, 60, 70, 60, 85, 100};
     vector<int> span = {0, 0, 0, 0, 0, 0, 0};
     stockSpanProblem(stock, span);
+*/
+
+/*    // Next greater element
+    vector<int> arr = {6, 8, 0, 1, 3};
+    vector<int> ans = {0, 0, 0, 0, 0};
+    nextGreaterElement(arr, ans);
+*/
+
+    // // Valid parenthesis
+    // string str = "(){}[]";
+    // validParenthesis(str);
+
+/*    // Duplicate parenthesis
+    string str1 = "((a+b))";
+    string str2 = "((a+b) + (c+d))";
+
+    cout<< isDuplicateParenthesis(str1)<< endl;
+    cout<< isDuplicateParenthesis(str2)<< endl;
+*/
+
+    vector<int> heights = {2, 1, 5, 6, 2, 3};
+    vector<int> heights2 = {2, 1, 5, 6, 7, 3};   
+    cout<< "max area = "<< maxAreaHistogram(heights)<< endl;
+    cout<< "max area = "<< maxAreaHistogram(heights2)<< endl;
+
 
     return 0;
 }
