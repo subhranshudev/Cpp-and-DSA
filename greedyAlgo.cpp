a5ee59cdd1214e22c0c1fe45d04e1a138f725fe3
subhranshudev <subhranshusekhar2509@gmail.com>
2025-10-03 20:47:03 +0530

    Pairs in C++ done

--- greedyAlgo.cpp
+++ greedyAlgo.cpp
@@ -1,5 +1,6 @@
 #include<iostream>
 #include<vector>
+#include<algorithm>
 using namespace std;
 
 int activitySelection(vector<int> start, vector<int> end){
@@ -22,11 +23,36 @@ int activitySelection(vector<int> start, vector<int> end){
     return count;
 }
 
+bool compare(pair<int, int> p1, pair<int, int> p2){
+    return p1.second < p2.second;   // ascending
+    // return p1.second > p2.second;   // descending
+}
+
 int main(){
-    // Activity Selection
+/*    // Activity Selection
     vector<int> start = {1, 3, 0, 5, 8, 5};
     vector<int> end = {2, 4, 6, 7, 9, 9};
 
     cout<<"Max activity = " << activitySelection(start, end);
+*/
+
+    vector<int> start = {0, 1, 2};
+    vector<int> end = {9, 2, 4};
+
+    vector<pair<int, int>> activity(3, make_pair(0, 0));
+    activity[0] = make_pair(0, 9);
+    activity[1] = make_pair(1, 2);
+    activity[2] = make_pair(2, 4);
+
+    for(int i = 0; i< activity.size(); i++){
+        cout<< "A"<< i << ": "<< activity[i].first << ", "<< activity[i].second<< endl;
+    }
+    cout<< "------sorted------ \n";
+
+    sort(activity.begin(), activity.end(), compare);
+    for(int i = 0; i< activity.size(); i++){
+        cout<< "A"<< i << ": "<< activity[i].first << ", "<< activity[i].second<< endl;
+    }
+
     return 0;
 }
