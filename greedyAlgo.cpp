02f02e9aec72e6334d24ea5a07e82f140468fecd
subhranshudev <subhranshusekhar2509@gmail.com>
2025-10-04 09:47:35 +0530

    Fractional knapsack done. code type 1

--- greedyAlgo.cpp
+++ greedyAlgo.cpp
@@ -28,6 +28,48 @@ bool compare(pair<int, int> p1, pair<int, int> p2){
     // return p1.second > p2.second;   // descending
 }
 
+bool compare2(pair<int, int> p1, pair<int, int> p2){
+    int cost1, cost2;
+    cost1 = p1.first / p1.second;
+    cost2 = p2.first / p2.second;
+
+    return cost1 > cost2; // sort in descending order , i.e. more valued items stored first
+}
+
+int fractionalKnapsack(vector<int> value, vector<int> weight, int w){   // TC = O(n logn)
+    int n = value.size();
+    int knapsackWeight = w;
+    vector<pair<double, int>> items(n, make_pair(0, 0));
+
+    for(int i = 0; i< n; i++){
+        items[i] = make_pair(value[i], weight[i]);
+    }
+
+    sort(items.begin(), items.end(), compare2);
+
+    int maxValue = 0;
+    for(int i = 0; i< n; i++){
+        double itemValue = items[i].first;
+        int itemWeight = items[i].second;
+
+        if(itemWeight <= knapsackWeight){
+            knapsackWeight -= itemWeight;
+            maxValue += itemValue;
+        } else{
+            double itemValuePerUnit = itemValue/itemWeight;
+            itemWeight = knapsackWeight;
+            itemValue = itemWeight*itemValuePerUnit;
+            
+            knapsackWeight = 0;
+            maxValue += itemValue;
+            break;
+        }
+    }
+
+    return maxValue;
+
+}
+
 int main(){
 /*    // Activity Selection
     vector<int> start = {1, 3, 0, 5, 8, 5};
@@ -36,7 +78,7 @@ int main(){
     cout<<"Max activity = " << activitySelection(start, end);
 */
 
-    vector<int> start = {0, 1, 2};
+/*    vector<int> start = {0, 1, 2};
     vector<int> end = {9, 2, 4};
 
     vector<pair<int, int>> activity(3, make_pair(0, 0));
@@ -53,6 +95,13 @@ int main(){
     for(int i = 0; i< activity.size(); i++){
         cout<< "A"<< i << ": "<< activity[i].first << ", "<< activity[i].second<< endl;
     }
+*/
+
+    // Fractional Knapsack
+    vector<int> value = {60, 100, 120};
+    vector<int> weight = {10, 20, 30};
+    int w = 50;
+    cout<< "Max Value = "<< fractionalKnapsack(value, weight, w);
 
     return 0;
 }
