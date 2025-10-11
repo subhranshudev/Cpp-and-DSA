119aa080bc27e57fb4e569c2dcc4120499f988bb
subhranshudev <subhranshusekhar2509@gmail.com>
2025-10-11 17:01:12 +0530

    Lowest Common Ancestor

--- binaryTrees.cpp
+++ binaryTrees.cpp
@@ -235,6 +235,64 @@ void kThLevel(Node* root, int k){   // O(n)-> because of helper function
     kThHelper(root, k, 1);
 }
 
+bool rootToNodePath(Node* root, int n, vector<int>& path){
+    if(root == NULL){
+        return false;
+    }
+
+    path.push_back(root->data);
+    if(root->data == n){
+        return true;
+    }
+
+    int isLeft = rootToNodePath(root->left, n, path);
+    int isRight = rootToNodePath(root->right, n, path);
+
+    if(isLeft || isRight){
+        return true;
+    }
+
+    path.pop_back();
+    return false;
+}
+int LCA(Node* root, int n1, int n2){  //  TC = O(n), SC = O(n)
+    vector<int> path1;
+    vector<int> path2;
+
+    rootToNodePath(root, n1, path1);    
+    rootToNodePath(root, n2, path2);  
+    
+    int lca = -1;
+    for(int i = 0, j = 0; i< path1.size(), j < path2.size(); i++, j++){
+        if(path1[i] != path2[j]){
+            return lca;
+        }
+
+        lca = path1[i];
+    }
+    
+    return lca;
+}
+
+Node* LCA2(Node* root, int n1, int n2){
+    if(root == NULL){
+        return NULL;
+    }
+
+    if(root->data == n1 || root->data == n2){
+        return root;
+    }
+
+    Node* leftLCA = LCA2(root->left, n1, n2);
+    Node* rightLCA = LCA2(root->right, n1,  n2);
+
+    if(leftLCA != NULL && rightLCA != NULL){
+        return root;
+    }
+
+    return leftLCA == NULL ? rightLCA : leftLCA;
+}
+
 int main(){
    // Build Tree from Preorder --> Time Complexity = O(n); n = size of preroder
     vector<int> nodes = {1, 2, 4, -1, -1, 5, -1, -1, 3, -1, 6, -1, -1};
@@ -291,7 +349,12 @@ int main(){
     //topView(root);
 
     // k th level of tree
-    kThLevel(root, 2);
+    // kThLevel(root, 2);
+
+    // Lowest Common Ancestor --> TC = O(n), SC = O(n)
+    int n1 = 4, n2 = 6;
+    cout<< "LCA = " << LCA(root, n1, n2)<< endl;
+    cout<< "LCA2 = " << LCA2(root, n1, n2)->data<< endl;
 
 
     return 0;
