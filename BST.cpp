195b40306343da7e8cc11d8992b512cfd78e1f12
subhranshudev <subhranshusekhar2509@gmail.com>
2025-10-20 12:40:54 +0530

    Search and Delete a Node

--- BST.cpp
+++ BST.cpp
@@ -45,11 +45,76 @@ void inorder(Node* root){
     inorder(root->right);
 }
 
+bool search(Node* root, int key){   // TC = O(height) 
+    if(root == NULL){
+        return false;
+    }
+
+    if(root->data == key){
+        return true;
+    }
+
+    if(key < root->data){
+        return search(root->left, key);
+    }else{
+        return search(root->right, key);
+    }
+}
+
+Node* getInorderSuccessor(Node* root){
+    while(root->left != NULL){
+        root = root->left;
+    }
+
+    return root;
+}
+
+Node* deleteNode(Node* root, int val){
+    if(root == NULL){
+        return NULL;
+    }
+
+    if(val < root->data){
+        root->left = deleteNode(root->left, val);
+    }else if(val> root->data){
+        root->right = deleteNode(root->right, val);
+    }else{
+        // root == val
+        // case-1: Leaf Node
+        if(root->left == NULL && root->right == NULL){
+            delete root;
+            return NULL;
+        }
+
+        // case-2: One Child
+        if(root->left == NULL || root->right == NULL){
+            return root->left == NULL ? root->right : root->left;
+        }
+
+        // Case-3: Two Children
+        Node* IS = getInorderSuccessor(root->right);
+        root->data = IS->data;
+        root->right = deleteNode(root->right, IS->data);
+    }
+
+    return root;
+}
+
 int main(){
+    //Build BST
     int arr[] = {5, 1, 3, 4, 2, 7};
     int arr2[] = {8, 5, 3, 1, 4, 6, 10, 11, 14};
     Node* root = buildBst(arr2, 9);
     inorder(root);
+    cout<< endl;
+
+    // Search in BST
+    //cout<< search(root, 6)<< endl;
+
+    // Delete Node in BST
+    deleteNode(root, 5);
+    inorder(root);
+
 
 
     return 0;
