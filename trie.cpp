eb27f5f28a23139784d6f162e03ceb9b452eb2c3
subhranshudev <subhranshusekhar2509@gmail.com>
2025-11-05 17:01:58 +0530

    Basics of Trie done

---
+++ trie.cpp
@@ -0,0 +1,66 @@
+#include<iostream>
+#include<unordered_map>
+#include<vector>
+#include<string>
+using namespace std;
+
+class Node{
+public:
+    unordered_map<char, Node*> children;
+    bool endOfWord;
+
+    Node(){
+        endOfWord = false;
+    }
+};
+
+class Trie{
+    Node* root;
+public:
+    Trie(){
+        root = new Node();
+    }
+
+    void insert(string key){    //TC =  O(L)
+        Node* temp = root;
+
+        for(int i = 0; i< key.size(); i++){ // Insert each char of string one by one
+            if(temp->children.count(key[i]) == 0){
+                temp->children[key[i]] = new Node();
+            }
+
+            temp = temp->children[key[i]];
+        }
+
+        temp->endOfWord = true;
+    }
+
+    bool search(string key){
+        Node* temp = root;
+
+        for(int i = 0; i < key.size(); i++){
+            if(temp->children.count(key[i])){
+                temp = temp->children[key[i]];
+            }else{
+                return false;
+            }
+        }
+
+        return temp->endOfWord;
+    }
+};
+
+int main(){
+    // Basics of Trie
+    vector<string> words = {"the", "a", "there", "their", "any", "thee"};
+    Trie trie;
+    for(int i = 0; i< words.size(); i++){
+        trie.insert(words[i]);
+    }
+
+    cout<< trie.search("the")<< endl;
+
+
+
+    return 0;
+}
