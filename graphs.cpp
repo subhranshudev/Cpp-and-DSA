08f1e8617193fb687a99a6d662a9fb8670614555
subhranshudev <subhranshusekhar2509@gmail.com>
2025-11-09 10:21:10 +0530

    hasPath Problem done

--- graphs.cpp
+++ graphs.cpp
@@ -82,9 +82,32 @@ public:
         }
     }
     void DFS(){
-        vector<bool> vis(7, false);
+        vector<bool> vis(V, false);
         DFSHelper(0, vis);
         cout<< endl;
+    }
+
+    bool hasPathHelper(int src, int dest, vector<bool> &vis){   // DFS approach TC = O(v+E)
+        if(src == dest){
+            return true;
+        }
+
+        vis[src] = true;
+        list<int> children = l[src];
+
+        for(int v : children){
+            if(!vis[v]){
+                if(hasPathHelper(v, dest, vis)){
+                    return true;
+                }
+            }
+        }
+
+        return false;
+    }
+    bool hasPath(int src, int dest){
+        vector<bool> vis(V, false);
+        return hasPathHelper(src, dest, vis);
     }
 
 };
@@ -112,7 +135,7 @@ int main(){
     graph.printWeighted();
 */
 
-    // Breadth First Search (BFS)
+/*    // Breadth First Search (BFS)
     Graph graph(7);
     graph.addEdge(0, 1);
     graph.addEdge(0, 2);
@@ -125,11 +148,28 @@ int main(){
 
     cout<< "BFS: ";
     graph.BFS();
+*/
 
-    // Depth First Search (DFS)
+/*    // Depth First Search (DFS)
     
     cout<< "DFS: ";
     graph.DFS();
+*/
+
+    // hasPath Problem
+    Graph graph(7);
+    graph.addEdge(0, 1);
+    graph.addEdge(0, 2);
+    graph.addEdge(1, 3);
+    graph.addEdge(2, 4);
+    graph.addEdge(3, 4);
+    graph.addEdge(3, 5);
+    graph.addEdge(4, 5);
+    graph.addEdge(5, 6);
+
+    cout<< graph.hasPath(0, 6)<< endl;
+
+
 
     return 0;
 }
